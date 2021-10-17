092ccd9d059cbd245ea0a8863c20e424a248a6d4
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-10-18 01:08:09 +0900

    dynamic positional encoding

--- vv_core_inference/make_yukarin_sosoa_forwarder.py
+++ vv_core_inference/make_yukarin_sosoa_forwarder.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import List, Optional
+import math
 
 import numpy
 import torch
@@ -12,6 +13,60 @@ from yukarin_sosoa.network.predictor import Predictor, create_predictor
 
 from vv_core_inference.utility import remove_weight_norm, to_tensor
 
+class RelPositionalEncoding(torch.nn.Module):
+    """Variant of espnet_pytorch_library/transformer/embedding.py#RelPositionalEncoding
+    copyright 2019 shigeki karita
+    apache 2.0  (http://www.apache.org/licenses/license-2.0)
+    """
+    def __init__(self, d_model, dropout_rate, max_len=5000):
+        """Construct an PositionalEncoding object."""
+        super().__init__()
+        assert d_model % 2 == 0
+        self.d_model = d_model
+        self.xscale = math.sqrt(self.d_model)
+        self.dropout = torch.nn.Dropout(p=dropout_rate)
+
+    def forward(self, x: torch.Tensor):
+        """Add positional encoding.
+
+        Args:
+            x (torch.Tensor): Input tensor (batch, time, `*`).
+
+        Returns:
+            torch.Tensor: Encoded tensor (batch, time, `*`).
+
+        """
+        # Suppose `i` means to the position of query vecotr and `j` means the
+        # position of key vector. We use position relative positions when keys
+        # are to the left (i>j) and negative relative positions otherwise (i<j).
+        pe_positive = torch.zeros(x.size(1), self.d_model//2, 2)
+        pe_negative = torch.zeros(x.size(1), self.d_model//2, 2)
+        position = torch.arange(0, x.size(1), dtype=torch.float32).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, self.d_model, 2, dtype=torch.float32)
+            * -(math.log(10000.0) / self.d_model)
+        )
+        pe_positive[:, :, 0] = torch.sin(position * div_term)
+        pe_positive[:, :, 1] = torch.cos(position * div_term)
+        pe_negative[:, :, 0] = torch.sin(-1 * position * div_term)
+        pe_negative[:, :, 1] = torch.cos(-1 * position * div_term)
+
+        pe_positive = pe_positive.view(x.size(1), self.d_model)
+        pe_negative = pe_negative.view(x.size(1), self.d_model)
+
+        # Reserve the order of positive indices and concat both positive and
+        # negative indices. This is used to support the shifting trick
+        # as in https://arxiv.org/abs/1901.02860
+        pe_positive = torch.flip(pe_positive, [0]).unsqueeze(0)
+        pe_negative = pe_negative[1:].unsqueeze(0)
+        pe = torch.cat([pe_positive, pe_negative], dim=1)
+
+        x = x * self.xscale
+        pos_emb = pe[
+            :,
+            pe.size(1) // 2 - x.size(1) + 1 : pe.size(1) // 2 + x.size(1),
+        ]
+        return self.dropout(x), self.dropout(pos_emb)
 
 def make_pad_mask(lengths: Tensor):
     bs = lengths.shape[0]
@@ -82,13 +137,14 @@ def make_yukarin_sosoa_wrapper(yukarin_sosoa_model_dir: Path, device) -> nn.Modu
         config = Config.from_dict(yaml.safe_load(f))
 
     predictor = create_predictor(config.network)
+    pe = predictor.encoder.embed[-1]
+    predictor.encoder.embed[-1] = RelPositionalEncoding(pe.d_model, pe.dropout.p) # Use my dynamic positional encoding version
     state_dict = torch.load(
         yukarin_sosoa_model_dir.joinpath("model.pth"), map_location=device
     )
     predictor.load_state_dict(state_dict)
     predictor.eval().to(device)
     predictor.apply(remove_weight_norm)
-    predictor.encoder.embed[0].pe = predictor.encoder.embed[0].pe.to(device)
     print("yukarin_sosoa loaded!")
     return WrapperYukarinSosoa(predictor)
 
