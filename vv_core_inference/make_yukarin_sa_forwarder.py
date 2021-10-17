a24fee7e510e388134abe5352387c8e739bcccea
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-10-17 17:54:25 +0900

    yukarin_sa: pure torch module

--- vv_core_inference/make_yukarin_sa_forwarder.py
+++ vv_core_inference/make_yukarin_sa_forwarder.py
@@ -21,14 +21,13 @@ class WrapperUniGRU(nn.Module):
 
 
 class WrapperYukarinSa(nn.Module):
-    def __init__(self, predictor: Predictor, device):
+    def __init__(self, predictor: Predictor):
         super().__init__()
         self.phoneme_embedder = predictor.phoneme_embedder
         self.speaker_embedder = predictor.speaker_embedder
         self.encoder = predictor.encoder
         self.ar_encoder = WrapperUniGRU(predictor.ar_encoder.rnn)
         self.post = predictor.post
-        self.device = device
 
     @torch.no_grad()
     def forward(
@@ -42,18 +41,12 @@ class WrapperYukarinSa(nn.Module):
         end_accent_phrase_list: Tensor,
         speaker_id: Optional[Tensor],
     ):
-        vowel_phoneme_list = to_tensor(vowel_phoneme_list, device=self.device).unsqueeze(0)
-        consonant_phoneme_list = to_tensor(consonant_phoneme_list, device=self.device).unsqueeze(0)
-        start_accent_list = to_tensor(start_accent_list, device=self.device).unsqueeze(0)
-        end_accent_list = to_tensor(end_accent_list, device=self.device).unsqueeze(0)
-        start_accent_phrase_list = to_tensor(
-            start_accent_phrase_list, device=self.device
-        ).unsqueeze(0)
-        end_accent_phrase_list = to_tensor(end_accent_phrase_list, device=self.device).unsqueeze(0)
-
-        if speaker_id is not None:
-            speaker_id = to_tensor(speaker_id, device=self.device)
-            speaker_id = speaker_id.reshape((-1,)).to(torch.int64)
+        vowel_phoneme_list = vowel_phoneme_list.unsqueeze(0)
+        consonant_phoneme_list = consonant_phoneme_list.unsqueeze(0)
+        start_accent_list = start_accent_list.unsqueeze(0)
+        end_accent_list = end_accent_list.unsqueeze(0)
+        start_accent_phrase_list = start_accent_phrase_list.unsqueeze(0)
+        end_accent_phrase_list = end_accent_phrase_list.unsqueeze(0)
 
         batch_size = 1
         length = vowel_phoneme_list.shape[1]
@@ -110,7 +103,7 @@ class WrapperYukarinSa(nn.Module):
             h = self.post(h)  # (batch_size, 1, length)
             f0 = h[:, 0, :]  # (batch_size, length)
 
-        return f0[0].cpu().numpy()  # (batch_size, length)
+        return f0[0]  # (length,)
 
 
 def make_yukarin_sa_forwarder(yukarin_sa_model_dir: Path, device):
@@ -125,5 +118,41 @@ def make_yukarin_sa_forwarder(yukarin_sa_model_dir: Path, device):
     predictor.eval().to(device)
     predictor.apply(remove_weight_norm)
     print("yukarin_sa loaded!")
+    wrapper = WrapperYukarinSa(predictor)
 
-    return WrapperYukarinSa(predictor, device)
+    def _dispatcher(
+        length: int,
+        vowel_phoneme_list: Tensor,
+        consonant_phoneme_list: Tensor,
+        start_accent_list: Tensor,
+        end_accent_list: Tensor,
+        start_accent_phrase_list: Tensor,
+        end_accent_phrase_list: Tensor,
+        speaker_id: Optional[Tensor],
+    ):
+        vowel_phoneme_list = to_tensor(vowel_phoneme_list, device=device)
+        consonant_phoneme_list = to_tensor(consonant_phoneme_list, device=device)
+        start_accent_list = to_tensor(start_accent_list, device=device)
+        end_accent_list = to_tensor(end_accent_list, device=device)
+        start_accent_phrase_list = to_tensor(
+            start_accent_phrase_list, device=device
+        )
+        end_accent_phrase_list = to_tensor(end_accent_phrase_list, device=device)
+
+        if speaker_id is not None:
+            speaker_id = to_tensor(speaker_id, device=device)
+            speaker_id = speaker_id.reshape((-1,)).to(torch.int64)
+
+        args = (
+            length,
+            vowel_phoneme_list,
+            consonant_phoneme_list,
+            start_accent_list,
+            end_accent_list,
+            start_accent_phrase_list,
+            end_accent_phrase_list,
+            speaker_id
+        )
+        return wrapper(*args).cpu().numpy()
+    return _dispatcher
+
